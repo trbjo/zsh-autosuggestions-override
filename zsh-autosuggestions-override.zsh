0972d00963087279ba0dfd3ea97c2acd92ac475e
Troels Bjørnskov <troels@bjoernskov.org>
2021-04-28 15:14:34 +0200

    Alight with prompt

--- zsh-autosuggestions-override.zsh
+++ zsh-autosuggestions-override.zsh
@@ -1,6 +1,8 @@
 # Accept the entire suggestion and execute it
 _zsh_autosuggest_execute() {
     if [ -z $BUFFER ]; then
+        pkill -f "/usr/bin/git -C ${PWD} fetch" > /dev/null 2>&1 && __last_check=$((10))
+        print -nP '\x1b[?25l' # hide cursor
         if [ "${LASTWIDGET}" == "autosuggest-execute" ] || [ "${LASTWIDGET}" == "goto_sublime_current_dir" ] && [ ${MYVAR} ]
         then
             print -Pn -- '\x1B[F\x1B[2K'
@@ -10,7 +12,9 @@ _zsh_autosuggest_execute() {
             clear
             MYVAR=1
         fi
-        zle redraw-prompt
+        preprompt
+        zle reset-prompt
+        print -nP -- '\x1b[?25h'      # show cursor
         return 0
     fi
 
