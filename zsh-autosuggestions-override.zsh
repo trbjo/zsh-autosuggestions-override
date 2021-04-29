65577668eed20f5ced864ebeded86b5350b48ce6
Troels Bjørnskov <troels@bjoernskov.org>
2021-04-29 13:32:06 +0200

    Adjust else clause to fit prompt

--- zsh-autosuggestions-override.zsh
+++ zsh-autosuggestions-override.zsh
@@ -1,7 +1,9 @@
 # Accept the entire suggestion and execute it
 _zsh_autosuggest_execute() {
     if [ -z $BUFFER ]; then
-        pkill -f "/usr/bin/git -C ${PWD} fetch" > /dev/null 2>&1 && __last_check=$((10))
+        git=$(pgrep -f "/usr/bin/git -c gc.auto=0 -C ${VCS_STATUS_WORKDIR} fetch --quiet --no-tags --recurse-submodules=no")
+        pid=($(pgrep -f "tail --pid=$git -f /dev/null"))
+        [ ! -z $pid ] && kill $(ps -o ppid= $pid) $pid
         print -nP '\x1b[?25l' # hide cursor
         if [ "${LASTWIDGET}" == "autosuggest-execute" ] || [ "${LASTWIDGET}" == "goto_sublime_current_dir" ] && [ ${MYVAR} ]
         then
@@ -14,19 +16,17 @@ _zsh_autosuggest_execute() {
         fi
         preprompt
         zle reset-prompt
-        print -nP -- '\x1b[?25h'      # show cursor
-        return 0
+    else
+        # Add the suggestion to the buffer
+        BUFFER="$BUFFER$POSTDISPLAY"
+
+        # Remove the suggestion
+        unset POSTDISPLAY
+
+        # Call the original `accept-line` to handle syntax highlighting or
+        # other potential custom behavior
+        _zsh_autosuggest_invoke_original_widget "accept-line"
     fi
-
-    # Add the suggestion to the buffer
-    BUFFER="$BUFFER$POSTDISPLAY"
-
-    # Remove the suggestion
-    unset POSTDISPLAY
-
-    # Call the original `accept-line` to handle syntax highlighting or
-    # other potential custom behavior
-    _zsh_autosuggest_invoke_original_widget "accept-line"
 }
 
 export KEYTIMEOUT=1
