bdfa68ac4220b1eccf84b8976b1fdc6ff9b177df
Troels Bjørnskov <troels@bjoernskov.org>
2021-05-24 22:29:49 +0200

    Remove git status

--- zsh-autosuggestions-override.zsh
+++ zsh-autosuggestions-override.zsh
@@ -15,7 +15,6 @@ _zsh_autosuggest_execute() {
         print -n '\x1b[?25l\033[2J\033[3J\033[H' # hide cursor and clear screen
         if [ "${LASTWIDGET}" == "autosuggest-execute" ] && [ ${MYVAR} ]
         then
-            git status --porcelain --short
             ls --color=auto --group-directories-first
             unset MYVAR
         else
