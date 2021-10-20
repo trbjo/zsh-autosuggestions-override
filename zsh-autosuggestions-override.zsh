5ad3eff3d952050b88b8158f21c82813e99b7556
Troels Bjørnskov <troels@bjoernskov.org>
2021-10-20 19:13:53 +0200

    Do ls on first run and prefer exa over ls if is found on the file system

--- zsh-autosuggestions-override.zsh
+++ zsh-autosuggestions-override.zsh
@@ -1,3 +1,9 @@
+if command -v exa > /dev/null 2>&1; then
+    _file_lister='exa'
+else
+    _file_lister='ls'
+fi
+
 # Accept the entire suggestion and execute it
 _zsh_autosuggest_execute() {
     if [[ $BUFFER ]]; then
@@ -13,17 +19,37 @@ _zsh_autosuggest_execute() {
     else
         control_git_sideeffects_preexec
         print -n '\033[2J\033[3J\033[H' # hide cursor and clear screen
-        if [ "${LASTWIDGET}" == "autosuggest-execute" ] && [ ${MYVAR} ]
-        then
-            ls --color=auto --group-directories-first
-            print
-            unset MYVAR
-        else
-            MYVAR=1
-        fi
+        $_file_lister --color=auto --group-directories-first
+        print
         preprompt
         zle reset-prompt
     fi
-}
 
+    _zsh_autosuggest_execute() {
+        if [[ $BUFFER ]]; then
+            # Add the suggestion to the buffer
+            BUFFER+="${POSTDISPLAY}"
+
+            # Remove the suggestion
+            [[ $POSTDISPLAY ]] && unset POSTDISPLAY || BUFFER="${BUFFER%%[[:blank:]]#}"
+
+            # Call the original `accept-line` to handle syntax highlighting or
+            # other potential custom behavior
+            _zsh_autosuggest_invoke_original_widget "accept-line"
+        else
+            control_git_sideeffects_preexec
+            print -n '\033[2J\033[3J\033[H' # hide cursor and clear screen
+            if [ "${LASTWIDGET}" == "autosuggest-execute" ] && [ ${MYVAR} ]
+            then
+                $_file_lister --color=auto --group-directories-first
+                print
+                unset MYVAR
+            else
+                MYVAR=1
+            fi
+            preprompt
+            zle reset-prompt
+        fi
+    }
+}
 bindkey -e '\e' autosuggest-execute
